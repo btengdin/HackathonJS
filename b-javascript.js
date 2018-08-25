98f5f22717023a30e01d19a9cc936038a22f94f2
btengdin <bridgettengdin@Bridgets-MacBook-Pro.local>
2018-08-25 10:22:22 -0700

    javascriptv1

---
+++ b-javascript.js
@@ -0,0 +1,23 @@
+let Q1A1= "yellow";
+let result = {q1: "hold",
+q2:"hold", q3:"hold"}
+
+
+function question1(){
+    
+if(Q1A1)
+}
+
+button01.addEventListener('click',question1);
+button02.addEventListener('click',addEventListener);
+button03.addEventListener('click',addEventListener);
+button04.addEventListener('click',addEventListener);
+button05.addEventListener('click',addEventListener);
+button06.addEventListener('click',addEventListener);
+button07.addEventListener('click',addEventListener);
+button08.addEventListener('click',addEventListener);
+button09.addEventListener('click',addEventListener);
+button10.addEventListener('click',addEventListener);
+button11.addEventListener('click',addEventListener);
+button12.addEventListener('click',addEventListener);
+
